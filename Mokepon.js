83b184c63b61844c00bf9441a01d35408d950340
CARLOS HERRERA <carlos09ballen@gmail.com>
2023-01-18 21:39:03 -0500

    estructura, identificacion y seleccion de botones

--- Mokepon.js
+++ Mokepon.js
@@ -1,1 +1,26 @@
-prompt("si esta funcionando")
+function iniciarJuego(){
+    var botonMascotaJugador = document.getElementById("boton-mascota")
+    botonMascotaJugador.addEventListener("click",seleccionarMascotaJugador)
+
+}
+
+function seleccionarMascotaJugador() {
+    var inputHipodoge = document.getElementById("Hipodoge")
+    var inputCapipepo = document.getElementById("Capipepo")
+    var inputRatigueya = document.getElementById("Ratigueya")
+
+    if (inputHipodoge.checked) {
+        alert("Seleccionaste a Hipodoge")
+    }
+    else if (inputCapipepo.checked) {
+        alert("Seleccionaste a Capipepo")
+    }
+    else if (inputRatigueya.checked) {
+        alert("Seleccionaste a Ratigueya")
+    }
+    else {
+        alert("NO PUEDES AVANZAR SIN SELECCIONAR UNA MASCOTA")
+    }
+}
+
+window.addEventListener("load", iniciarJuego)
